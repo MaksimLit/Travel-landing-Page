b2a5f9b74b4d562838d6e7918c8935b76c794dac
MaksimLit <46899865+MaksimLit@users.noreply.github.com>
2020-05-05 17:17:13 +0300

    adapted menu comleted

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@
         }
     };
 }());
+
+// Burger handler
+(function() {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burger.addEventListener('click', () => {
+        menu.classList.add('header__nav_active')
+    })
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active')
+    })
+}());
